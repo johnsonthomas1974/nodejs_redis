05bc23f2d02b43567a4073e7ed4befafa8586687
johnsonthomas1974 <johnsonthomas1974@gmail.com>
2015-09-15 14:46:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var redis = require('redis');
-var client = redis.createClient(6379,"172.17.42.1");
+var host_ip, port_nu;
+console.log( process.env.RDS_PORT_6379_TCP_ADDR );
+if (process.env.RDS_PORT_6379_TCP_ADDR){
+   console.log("Assigned");
+   host_ip =process.env.RDS_PORT_6379_TCP_ADDR;
+} else {
+   host_ip = "172.17.42.1";
+}
+if (process.env.RDS_PORT_6379_TCP_PORT){
+   port_nu= process.env.RDS_PORT_6379_TCP_PORT;
+} else {
+   port_nu= 6379;
+}
+console.log (host_ip + port_nu);
+
+//var client = redis.createClient(6379,"172.17.42.1");
+var client = redis.createClient(port_nu, host_ip);
 client.on('connect', function() {
     console.log('connected');
 });
@@ -21,4 +37,4 @@ client.set(param_name,param_value, function(err, reply) {
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log('Running on http://localhost:' + PORT); 
